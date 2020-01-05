ae10239ee6c775db40fe8d531d59a855e6df46d6
k-a-l <52395887+k-a-l@users.noreply.github.com>
2020-01-05 12:59:51 +0530

    Create asciiart.py

---
+++ sapt/lib/asciiart.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+'''
+Copyright (C) 2020, kal.
+'''
+# ascii art of sapt
+def asciiArt():
+	print( '''
+
+	                    ___    __    ____  ____
+	                   / __)  /__\  (  _ \(_  _)
+		                 \__ \ /(__)\  )___/  )(
+	                   (___/(__)(__)(__)   (__)
+
+	          """Semi Automated Penetration Testing""" by kal
+	''')
