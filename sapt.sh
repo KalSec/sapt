7394b68b143fb7c201643058f751b0e6fc91068b
k-a-l <52395887+k-a-l@users.noreply.github.com>
2020-01-05 12:00:33 +0530

    Update sapt.sh

--- sapt.sh
+++ sapt.sh
@@ -6,20 +6,23 @@ echo "                    ___    __    ____  ____                    "
 echo "                   / __)  /__\  (  _ \(_  _)                   "
 echo "                   \__ \ /(__)\  )___/  )(                     "
 echo "                   (___/(__)(__)(__)   (__)                    "
-echo "								     "
+echo "                                                               "
 echo "          """Semi Automated Penetration Testing""" by kal      "
-echo "								     "
+echo "                                                               "
 
 # checking for root/sudo
 
+echo "[+] Checking for root"
+
 if [[ $EUID -ne 0 ]]; then
-        echo "SAPT must be run as root"; exit 1
+        echo "[-] SAPT must be run as root"; exit 1
 fi
 
 # checking for requirements
  
-requirements=(nmap nc foo)
+echo "[+] checking for requirements"
+
+requirements=(nmap nc foo bar)
 for i in "${requirements[@]}"; do
         command -v $i >/dev/null || { echo "I require $i but it's not installed."; }
 done
-
