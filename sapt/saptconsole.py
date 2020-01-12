b072410c192d9c28de303455cf8221d9fe7438ee
k-a-l <52395887+k-a-l@users.noreply.github.com>
2020-01-12 17:44:46 +0530

    Update saptconsole.py

--- sapt/saptconsole.py
+++ sapt/saptconsole.py
@@ -4,18 +4,23 @@
 Copyright (C) 2020, kal.
 '''
 
+import sys
 from os import system
 from lib.asciiart import *
-from lib.args import *
+from argparse import ArgumentParser
 
+usage = "sapt [options] "
+Version = "sapt 0.0.1"
+parser = ArgumentParser(usage=usage, description="SAPT is an penetration testing tool used to automate the normal or common taks", epilog="Hope you are happy with this tool.")
+parser.version = Version
+parser.add_argument('-q', '--quiet', action='store_true', help="Disables the logo")
+parser.add_argument('-V', '--verbose', action='store_true', dest='verbose', help="Displays more information.")
+parser.add_argument('-e', '--enumerate', metavar='', help="Displays more information.")
+parser.add_argument('--version', action='version')
+args = parser.parse_args()
+system("clear")
 # printing Asciiart
-asciiArt()
-
+if not args.quiet:
+    asciiArt()
 # Checking for root
 isRoot()
-
-# Checking the arguments
-arguments()
-
-# set the console
-
