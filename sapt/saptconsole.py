79b15e26a5d5411c99e9c883ef72dab9fb0d5f58
k-a-l <52395887+k-a-l@users.noreply.github.com>
2020-01-12 16:34:58 +0530

    Add files via upload

---
+++ sapt/saptconsole.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+'''
+Copyright (C) 2020, kal.
+'''
+
+from os import system
+from lib.asciiart import *
+from lib.args import *
+
+# Clear the screen
+system("clear")
+
+# printing Asciiart
+asciiArt()
+
+# Checking for root
+isRoot()
+
+# Checking the arguments
+arguments()
+
+# set the console
+
